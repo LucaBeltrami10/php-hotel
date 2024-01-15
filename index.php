5b53764e7ba80343b70257e0852b859e320fc71c
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-15 15:15:27 +0100

    nuovo file php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-hotel</title>
+</head>
+<body>
+    
+</body>
+</html>
