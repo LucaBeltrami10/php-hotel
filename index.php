a0ffd23fa2e04596e881c7dbd067ef7104c99aa0
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-15 15:37:32 +0100

    inserito ciclo for each e stampato in html contenuto array

--- index.php
+++ index.php
@@ -52,6 +52,53 @@ $hotels = [
 </head>
 <body>
 
+<div>
+  <ul>
+  <?php foreach($hotels as $hotel){ ?>
+  <li> 
+    <p><?php echo $hotel['name'] ?></p>
+    <p><?php echo $hotel['description'] ?></p>
+    <p><?php echo $hotel['parking'] ?></p>
+    <p><?php echo $hotel['vote'] ?></p>
+    <p><?php echo $hotel['distance_to_center'] ?></p>
+    
+  </li>
+  <?php } ?>
+  </ul>
+    
+</div>
+
+
+<!-- <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table> -->
 
     
 </body>
